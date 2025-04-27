03be25f3ea950de7144cc54519ed6a8a99ff12fd
kon <bastenkonstantin@gmail.com>
2025-04-27 17:53:23 +0200

    next.config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
-  // disable in dev
   disable: process.env.NODE_ENV === 'development',
-})
+});
 
 module.exports = withPWA({
   reactStrictMode: true,
-  // any other Next.js config...
-})
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false,  // use true if this redirect is permanent (SEO benefit)
+      },
+    ];
+  },
+});
